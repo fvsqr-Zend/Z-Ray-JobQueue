ace3fdc4fdfbcea8d825c23ca763a9bd4e869eed
jan <jan@5square.de>
2017-09-13 16:35:40 +0200

    initial commit for new branch

--- zray/zray.php
+++ zray/zray.php
@@ -8,23 +8,50 @@ $zre->setMetadata(array(
     'actionsBaseUrl' => $_SERVER['REQUEST_URI'] 
 ));
 
+function shutdown() {
+}
+
 if (extension_loaded('Zend Job Queue')) {
  
     $q = new \ZendJobQueue();
-        
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'JobQueue.php';
     
-    $q = new \ZendJobQueue();
     $jq = new JobQueue();
     
-    $zre->setEnabledAfter('ZendJobQueue::ZendJobQueue');
-    
-    $zre->traceFunction(
-       'ZendJobQueue::createHttpJob', 
-        function() {},
-        array(
-            $jq,
-            'afterJobStart'
-        )
-    );
+    if ($q->getCurrentJobId()) {
+        
+        $zre->setEnabledAfter('ZendServerJobQueue\shutdown');
+        
+        register_shutdown_function('ZendServerJobQueue\shutdown');
+        $zre->traceFunction(
+            'ZendJobQueue::setCurrentJobStatus',
+            function() {},
+            array(
+                $jq,
+                'workerStatus'
+            )
+        );
+        
+        $zre->traceFunction(
+            'ZendServerJobQueue\shutdown',
+            function() {},
+            array(
+                $jq,
+                'workerShutdown'
+            )
+        );
+    }
+    else {
+        
+        $zre->setEnabledAfter('ZendJobQueue::ZendJobQueue');
+        
+        $zre->traceFunction(
+           'ZendJobQueue::createHttpJob', 
+            function() {},
+            array(
+                $jq,
+                'afterJobStart'
+            )
+        );
+    }
 }
